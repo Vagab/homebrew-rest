0fb74e3e790d94715c2c2f3ae41c655eef16287d
barseek <sergey.b@hey.com>
2023-05-28 20:52:48 +0200

    Change sha256

--- Formula/rest.rb
+++ Formula/rest.rb
@@ -2,7 +2,7 @@ class Rest < Formula
   desc 'Control screen dimming to rest your eyes'
   homepage 'https://github.com/Vagab/rest'
   url 'https://github.com/Vagab/rest/archive/v0.1.0.tar.gz'
-  sha256 'ca498e4985c21405e24d77db26045ac22df559fcdb3d81129cd5cc27a3d77967'
+  sha256 '9550225591b29d38e276cd93d86abc417892df30bd80fbfebd617e0053f49520'
   head 'https://github.com/Vagab/rest.git'
 
   depends_on 'brightness' => :build
