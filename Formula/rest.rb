5bb500dcf6271409c516e553d067ff1f1c8b4fa4
barseek <sergey.b@hey.com>
2023-05-29 14:07:43 +0200

    Update version

--- Formula/rest.rb
+++ Formula/rest.rb
@@ -1,7 +1,7 @@
 class Rest < Formula
   desc 'Control screen dimming to rest your eyes'
   homepage 'https://github.com/Vagab/rest'
-  url 'https://github.com/Vagab/rest/archive/v0.1.1.tar.gz'
+  url 'https://github.com/Vagab/rest/archive/v0.1.2.tar.gz'
   sha256 'c2336abe232d67fe421f85158456dfb2f9f1c3fe815b44045c99298de0bf748f'
   head 'https://github.com/Vagab/rest.git'
 
